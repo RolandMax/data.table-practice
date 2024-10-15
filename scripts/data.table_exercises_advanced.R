8ce55704bdf4936b607fa7182ffdbdf1cdb64f03
RolandMax <rolandmaximillianholzinger@gmail.com>
2024-10-15 21:12:34 +0200

    started  advanced exercises

--- scripts/data.table_exercises_advanced.R
+++ scripts/data.table_exercises_advanced.R
@@ -3,34 +3,45 @@
 
 # Load required library and data
 library(data.table)
-flights <- fread("https://raw.githubusercontent.com/Rdatatable/data.table/master/vignettes/flights14.csv")
+dt <- fread("https://raw.githubusercontent.com/Rdatatable/data.table/master/vignettes/flights14.csv")
 
 # Exercise 1: Using :=
 # Add a new column 'is_delayed' that is TRUE if dep_delay > 15, FALSE otherwise
+dt[, 
+   ':=' (is_delayed = ifelse(dep_delay, TRUE, FALSE))]
 
+str(dt)
 
 # Exercise 2: Multiple := operations
 # In a single operation, add two new columns:
 # 'total_delay': sum of dep_delay and arr_delay
 # 'speed_mph': distance / air_time * 60
+dt[,
+   c("total_delay", "speed_mph") := .(dep_delay + arr_delay, distance/(air_time * 60))]
+
+
 
 
 # Exercise 3: :=  with by
 # For each origin, calculate and add a new column 'avg_delay_at_origin'
 # containing the average departure delay for that origin
+dt[,
+   ':='(avg_delay_at_origin = mean(dep_delay)),
+   by = .(origin)]
 
 
 # Exercise 4: Setting a key
 # Set 'origin' and 'dest' as the key for the flights data.table
-
+setkey(dt, origin, dest)
+key(dt)
 
 # Exercise 5: Using a key for fast subsets
 # Using the key set in Exercise 4, quickly subset all flights from JFK to LAX
-
+dt[c("JFK", "LAX")]
 
 # Exercise 6: Sorting with key
 # Sort the flights first by origin, then by destination, using the key
-
+dt[keyby = .(origin, dest)]
 
 # Exercise 7: Adding a secondary index
 # Add a secondary index on the 'carrier' column
